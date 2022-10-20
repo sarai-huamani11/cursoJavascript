158d35d578ba98443346885cd4d7438b02262574
selem <saraiselemhuamanihuamani.09@gmail.com>
2022-10-20 11:06:49 -1200

    trabajo realizado con quokka de maykol y selem

--- 3.operadores/ejercicios.js
+++ 3.operadores/ejercicios.js
@@ -1,19 +1,23 @@
 // 1.-Operadores aritméticos(maykol y selem)
-precio = 128 //introduzco un 128 en la variable precio 
-unidades = 10 //otra asignación, luego veremos operadores de asignación 
-factura = precio * unidades //multiplico precio por unidades, obtengo el valor factura 
-resto = factura % 3 //obtengo el resto de dividir la variable factura por 3 
-precio++ //incrementa en una unidad el precio (ahora vale 129)
-
+a=1
+b=2
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
 //2.- Operadores de asignación (maykol y selem)
 ahorros = 7000 //asigna un 7000 a la variable ahorros 
-ahorros += 3500 //incrementa en 3500 la variable ahorros, ahora vale 10500 
-ahorros /= 2 //divide entre 2 mis ahorros, ahora quedan 5250
+incremento= 3500 //incrementa en 3500 la variable ahorros, ahora vale 10500 
+subtotal=ahorros +incremento 
+console.log(ahorros + incremento)
+subtotal / 2 //divide entre 2 mis ahorros, ahora quedan 5250
+console.log(subtotal/2)
+
 
 // 3.- Operadores de cadenas  (maykol y selem)
-cadena1 = "hola" 
+cadena1 = "hola"
 cadena2 = "mundo" 
-cadenaConcatenada = cadena1 + cadena2 //cadena concatenada vale "holamundo"
+console.log(cadena1 + cadena2) //cadena concatenada vale "holamundo"
 
 // 4.- Operadores lógicos (maykol y selem)
 miBoleano = true 
@@ -21,25 +25,16 @@ miBoleano = !miBoleano //miBoleano ahora vale false
 tengoHambre = true 
 tengoComida = true 
 comoComida = tengoHambre && tengoComida
+console.log(tengoComida&&tengoHambre)
 
 // 5.- Operadores condicionales (maykol y selem)
- 
-5==5 //verdadero
-6==8 //falso
-
-21!=5 //verdadero
-6!=6 //falso
-
-5<9 //verdadero
-6<2 //falso
-
-6>5 //verdadero
-6>6 //falso
-
-5<=5 //verdadero
-6<=10 //falso
-
-5>=5 //verdadero
-6>=8 //falso
+a=4
+b=6
+console.log(a==b)
+console.log(a!=b)
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
 
 
