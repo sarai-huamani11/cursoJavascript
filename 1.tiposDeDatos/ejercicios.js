5f5c74381ec95771959bd80af9e19438ba9074b6
selem <saraiselemhuamanihuamani.09@gmail.com>
2022-10-19 14:52:51 -1200

    tarea realizada

--- 1.tiposDeDatos/ejercicios.js
+++ 1.tiposDeDatos/ejercicios.js
@@ -1,2 +1,4 @@
 // averiguar el metodo de javascript
 // que me permite saber que tipo de dato tengo
+// RESPUESTA
+//el metodo es el operador typeof, que devuelve una cadena de texto que describe el tipo del operador que estamos comprobando.
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,1 +1,3 @@
 // avergiuar las diferencias entre let y var
+//let te permite declarar variables limitando su alcance (scope) al bloque, declaración, o expresión donde se está usando. 
+// var la cual define una variable global o local en una función sin importar el ámbito del bloque.
