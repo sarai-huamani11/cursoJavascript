96ca6115bb66948ab588b693b4bad42f6efd0098
selem <saraiselemhuamanihuamani.09@gmail.com>
2022-10-20 10:26:51 -1200

    trabajo realizado grupal maykol y selem

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,45 @@
+// 1.-Operadores aritméticos(maykol y selem)
+precio = 128 //introduzco un 128 en la variable precio 
+unidades = 10 //otra asignación, luego veremos operadores de asignación 
+factura = precio * unidades //multiplico precio por unidades, obtengo el valor factura 
+resto = factura % 3 //obtengo el resto de dividir la variable factura por 3 
+precio++ //incrementa en una unidad el precio (ahora vale 129)
+
+//2.- Operadores de asignación (maykol y selem)
+ahorros = 7000 //asigna un 7000 a la variable ahorros 
+ahorros += 3500 //incrementa en 3500 la variable ahorros, ahora vale 10500 
+ahorros /= 2 //divide entre 2 mis ahorros, ahora quedan 5250
+
+// 3.- Operadores de cadenas  (maykol y selem)
+cadena1 = "hola" 
+cadena2 = "mundo" 
+cadenaConcatenada = cadena1 + cadena2 //cadena concatenada vale "holamundo"
+
+// 4.- Operadores lógicos (maykol y selem)
+miBoleano = true 
+miBoleano = !miBoleano //miBoleano ahora vale false 
+tengoHambre = true 
+tengoComida = true 
+comoComida = tengoHambre && tengoComida
+
+// 5.- Operadores condicionales (maykol y selem)
+ 
+5==5 //verdadero
+6==8 //falso
+
+21!=5 //verdadero
+6!=6 //falso
+
+5<9 //verdadero
+6<2 //falso
+
+6>5 //verdadero
+6>6 //falso
+
+5<=5 //verdadero
+6<=10 //falso
+
+5>=5 //verdadero
+6>=8 //falso
+
+
---
+++ 3.operadores/index.js
@@ -0,0 +1,60 @@
+// jhongutierrezmarca@gmail.com
+
+
+// CONCEPTO DE OPERADORES (MAYKOL)
+//Los operadores permiten manipular el valor de las variables, realizar operaciones matemáticas con sus valores y comparar diferentes variables. 
+
+// CONCEPTO DE OPERADORES (SELEM)
+// Los operadores permiten a los programas realizar cálculos complejos y tomar decisiones lógicas en función de comparaciones y otros tipos de condiciones.
+
+// TIPOS DE OPERADORES (MAYKOL Y SELEM )
+
+// 1.-Operadores aritméticos :
+// (MAYKOL) Son los utilizados para la realización de operaciones matemáticas simples como la suma, resta o multiplicación. 
+// (SELEM) se usan para calcular un valor de dos o más números, o cambiar el signo de un número de positivo a negativo o viceversa.
+//+ Suma de dos valores
+//- Resta de dos valores
+//* Multiplicación de dos valores
+//    / División de dos valores
+//% El resto de la división de dos números (3%2 devolvería 1, el resto de dividir 3 entre 2)
+//++ Incremento en una unidad, se utiliza con un solo operando
+//-- Decremento en una unidad, utilizado con un solo operando
+
+//2.- Operadores de asignación: 
+// (MAYKOL) Sirven para asignar valores a las variables.
+// (SELEM)Este operador se utiliza para guardar un valor específico en una variable.
+//= Asignación. 
+//+= Asignación con suma. 
+//-= Asignación con resta
+//*= Asignación de la multiplicación
+// /= Asignación de la división
+// %= Se obtiene el resto y se asigna
+
+// 3.- Operadores de cadenas: 
+//(MAYKOL) Las cadenas de caracteres, o variables de texto, también tienen sus propios operadores para realizar acciones típicas sobre cadenas.
+// (SELEM) sirven para concatenar cadenas u objetos.
+//+ Concatena dos cadenas, pega la segunda cadena a continuación de la primera.
+
+// 4.- Operadores lógicos: 
+// (MAYKOL) Estos operadores sirven para realizar operaciones lógicas, que son aquellas que dan como resultado un verdadero o un falso.Son el verdadero (true) y el falso (false). 
+// (SELEM) son imprescindibles para realizar aplicaciones complejas, ya que se utilizan para tomar decisiones sobre las instrucciones que debería ejecutar el programa en función de ciertas condiciones.
+//! Operador NO o negación. Si era true pasa a false y viceversa.
+// && Operador Y, si son los dos verdaderos vale verdadero.
+// || Operador O, vale verdadero si por lo menos uno de ellos es verdadero.
+
+// 5.- Operadores condicionales: 
+// (MAYKOL) Sirven para realizar expresiones condicionales todo lo complejas que deseemos.
+// (SELEM) Es aquel que compara dos datos y nos indica la relación que hay entre ellos.
+// == Comprueba si dos valores son iguales
+// != Comprueba si dos valores son distintos
+// > Mayor que, devuelve true si el primer operando es mayor que el segundo
+// < Menor que, es true cuando el elemento de la izquierda es menor que el de la derecha
+// >= Mayor igual
+// <= Menor igual
+
+// CONCLUSION  (SELEM)
+// En conclusion los operadores junto con los operandos forman una expresión, que es una fórmula que define el cálculo de un valor. 
+// CONCLUSION (MAYKOL)
+// En conclusion los operadores pueden ser constantes, variables o llamadas a funciones, siempre que éstas devuelvan algún valor.
+
+ 
